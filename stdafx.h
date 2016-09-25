d24ab9c2ddbc37de90c5d36f1e45ebc5c5423395
Centauria <centauria_v_c@163.com>
2016-09-26 07:18:15 +0800

    delete comments in stdafx.h

--- stdafx.h
+++ stdafx.h
@@ -1,8 +1,3 @@
-// stdafx.h : 标准系统包含文件的包含文件，
-// 或是经常使用但不常更改的
-// 特定于项目的包含文件
-//
-
 #pragma once
 
 
@@ -15,7 +10,3 @@
 #include <time.h>
 #include <math.h>
 #include "fft.h"
-
-
-
-// TODO:  在此处引用程序需要的其他头文件
