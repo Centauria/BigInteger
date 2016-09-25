7c9cbc3ec48e441db5d3ba6bfd157ca768ca8d2e
Centauria <centauria_v_c@163.com>
2016-09-26 07:23:08 +0800

    eee

--- fft.h
+++ fft.h
@@ -1,7 +1,4 @@
-﻿#ifndef __FFT_H__
-#define __FFT_H__
-
-typedef double type;
+﻿typedef double type;
 
 typedef struct complex{
 	type real;
@@ -11,13 +8,11 @@ typedef struct complex{
 #define PI 3.1415926535897932384626433832795028841971
 
 
-void conjugate_complex(int n, complex in[], complex out[]);
-void c_plus(complex a, complex b, complex *c);
-void c_mul(complex a, complex b, complex *c);
-void c_sub(complex a, complex b, complex *c);
-void c_div(complex a, complex b, complex *c);
-void fft(int N, complex f[]);
-void ifft(int N, complex f[]);
-void c_abs(complex f[], type out[], int n);
-
-#endif
+void conjugate_complex(int n, Complex in[], Complex out[]);
+void c_plus(Complex a, Complex b, Complex *c);
+void c_mul(Complex a, Complex b, Complex *c);
+void c_sub(Complex a, Complex b, Complex *c);
+void c_div(Complex a, Complex b, Complex *c);
+void fft(int N, Complex f[]);
+void ifft(int N, Complex f[]);
+void c_abs(Complex f[], type out[], int n);
